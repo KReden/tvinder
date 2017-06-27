42cd8101237fabf89561c6dc082d009e2d0cb9e4
Marshall Shen <fengyuan.shen@C02PTZ9GG8WP-9219.local>
2017-06-27 16:19:46 -0500

    add animation

--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="movies">
     <div class="movie-poster-container">
-      <img class="movie-poster" v-bind:src="extractImageUrl" v-bind:key="extractImageUrl">
+      <div class="transition-container">
+        <transition name="fade">
+          <img class="movie-poster" v-bind:src="extractImageUrl" v-bind:key="extractImageUrl">
+        </transition>
+      </div>
       <div class="movie-name">{{ extractImageName }}</div>
     </div>
   </div>
@@ -45,11 +49,35 @@ export default {
   object-fit: cover;
   margin: 0 auto;
 }
-.movie-poster {
+
+.transition-container, .movie-poster {
   height: 30em;
 }
+
+.movie-poster {
+  position: absolute;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+}
+
 .movie-name {
   padding: .5em;
   font-size: 1.25em;
 }
+
+
+.fade-enter-active {
+  transition: all .8s ease;
+}
+.fade-leave-active {
+  transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+.fade-leave-to {
+  transform: translateY(-100px);
+}
+
 </style>
